b750f263a52afc1f8d289340ec3495acb9757470
DayannaGarcia <yayisgarcia15@gmail.com>
2018-08-24 17:16:15 -0500

    operaciones

--- Ejercicio/Ejercicio/Ejercicio/Propinas.xaml.cs
+++ Ejercicio/Ejercicio/Ejercicio/Propinas.xaml.cs
@@ -12,6 +12,24 @@ namespace Ejercicio
 		public MainPage()
 		{
 			InitializeComponent();
+            btnCalcular.Clicked += btnClicked;
 		}
-	}
+
+        private void btnClicked(object sender, EventArgs e)
+        {
+            double Total = double.Parse(tot.Text);
+            double Propina = double.Parse(prop.Text);
+            double NumeroPersona = double.Parse(numPer.Text);
+
+            double ResPropina = (Total*Propina)/100;
+            double ResTotal = ResPropina + Total;
+            double ResPropPersona = ResPropina / NumeroPersona;
+            double ResTotPersona = ResTotal / NumeroPersona;
+
+            txtPropina.Detail = Propina.ToString("C");
+            txtTotal.Detail = ResTotal.ToString("C");
+            txtPropinaPer.Detail = ResPropPersona.ToString("C");
+            txtTotalPer.Detail = ResTotPersona.ToString("C");
+        }
+    }
 }
